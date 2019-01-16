d73d521fd3a4e0b831545d7db38996efdfbcfcb4
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-01-16 17:09:12 +0100

    Update cat.js

--- command/cat.js
+++ command/cat.js
@@ -1,8 +1,10 @@
 const { get } = require('snekfetch')
 exports.run = async (Discord, client, message, args) => {
+    let roleColor = message.member.highestRole.color
     try {
         get('https://aws.random.cat/meow').then(res => {
             const embed = new Discord.RichEmbed()
+            .setColor(roleColor)
             .setImage(res.body.file)
             return message.channel.send({embed});
         });
