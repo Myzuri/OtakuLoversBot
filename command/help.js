fdddd73659e3c7ce6e4167601b56dd9d940398c4
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-01-16 17:09:40 +0100

    Update help.js

--- command/help.js
+++ command/help.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, args, message) => {
-
+    let roleColor = message.member.highestRole.color
     let user = message.author;
     let sembed = new Discord.RichEmbed()
     .setTitle("My commands")
     .addField('Fun', '`avatar`, `ascii`, `calc`, `ping`, `cat`, `shifumi`, `hug`, `8ball`, `kiss`')
     .addField('Usefull', '`userinfo`, `serverinfo`, `purge`')
-    .setColor('RANDOM')
+    .setColor(roleColor)
 
     //.setTimestamp(message.guild.createdAt);
     return message.channel.send(sembed);
