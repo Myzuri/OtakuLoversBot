406fea74f8728233c4b2f75ddc49664d779078b0
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-01-16 17:03:32 +0100

    Update avatar.js

--- command/avatar.js
+++ command/avatar.js
@@ -6,13 +6,14 @@ module.exports.run = async (Discord, client, message, args) => {
 	let user = message.mentions.users.first();
 	let search = args.join(' ');
 	let tags = client.users.find(u => u.username === search);
+	let roleColor = message.member.highestRole.color;
 	var author; // mention to get avatar, or type username
 	
 		if(user){
 			var author = user;
 
 			let embed = new Discord.RichEmbed()
-			.setColor('RANDOM')
+			.setColor(roleColor)
 			.setDescription(`${user.tag}'s avatar`)
 			.setImage(`${user.avatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
@@ -21,14 +22,14 @@ module.exports.run = async (Discord, client, message, args) => {
 			var author = tags;
 
 			let embed = new Discord.RichEmbed()
-			.setColor('RANDOM')
+			.setColor(roleColor)
 			.setDescription(`${tags.tag}'s avatar`)
 			.setImage(`${tags.avatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
 		} else {
 			var author = message.author;
 			let embed = new Discord.RichEmbed()
-			.setColor('RANDOM')
+			.setColor(roleColor)
 			.setDescription("Your avatar")
 			.setImage(`${author.displayAvatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${message.author.tag} used avatar`))
