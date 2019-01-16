43d703a70d8feba3fbc4e5cb94bb04a344f00b28
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-01-16 17:15:15 +0100

    Update userinfo.js

--- command/userinfo.js
+++ command/userinfo.js
@@ -1,14 +1,16 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, args, message) => {
-
+      
+      let roleColor = message.member.highestRole.color;
       let user = message.mentions.users.first() || message.author;
       let embed = new Discord.RichEmbed()
-      .setColor('RANDOM')
+      .setColor(roleColor)
       .setTitle("Here some info about " + `${user.tag}`)
       .setThumbnail(user.displayAvatarURL)
       .addField("Etat", user.presence.status, true)
       .addField("ID", user.id, true)
+      .addField('Role', user.roles.map(u=>u.name))
       .setDescription('[full image]('+user.avatarURL+')')
       //.addField("Joue a ", "\`" + user.presence.game.name + "\`")
       .setFooter("Account created at : ")
