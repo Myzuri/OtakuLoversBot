381b0f9c6d200db44dc7ccf5d753928dcf9d6944
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-01-16 07:37:31 +0100

    Update and rename dsay.js to say.js

--- command/say.js
+++ command/say.js
@@ -2,7 +2,7 @@ module.exports.run = async (Discord, client, message, args) =>  {
 
 	var say = args.join(' ')
 
-	if(!say [0] || say[0 == 'help']) return message.channel.send("```!dsay [content] \n cannot be empty | don't even try !dsay [!sday]```")
+	if(!say [0] || say[0 == 'help']) return message.channel.send("```!say [content] \n cannot be empty | don't even try !dsay [!say]```")
 	
 		message.delete(1000)
 		return message.channel.send(say)
