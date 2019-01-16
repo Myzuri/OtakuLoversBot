3144d44fae2c6e7778dfc2c6be94ff6645d173ac
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-01-16 07:38:55 +0100

    Update help.js

--- command/help.js
+++ command/help.js
@@ -5,8 +5,8 @@ module.exports.run = async (client, args, message) => {
     let user = message.author;
     let sembed = new Discord.RichEmbed()
     .setTitle("My commands")
-    .addField('Fun', '`avatar`, `ascii`, `calc`, `ping`, `cat`, `shifumi`, `hug`, `8ball`')
-    .addField('Usefull', '`userinfo`, `serverinfo`')
+    .addField('Fun', '`avatar`, `ascii`, `calc`, `ping`, `cat`, `shifumi`, `hug`, `8ball`, `kiss`')
+    .addField('Usefull', '`userinfo`, `serverinfo`, `purge`')
     .setColor('RANDOM')
 
     //.setTimestamp(message.guild.createdAt);
