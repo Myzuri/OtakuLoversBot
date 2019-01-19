091d7e84b8b1ececa46f43edf62ad3b3a95dea45
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-01-19 13:47:18 +0100

    Update rainbow.js

--- command/rainbow.js
+++ command/rainbow.js
@@ -1,6 +1,9 @@
 
 module.exports.run = async (Discord, client, message, args) => {
 
+    let giverole = message.mentions.members.first();
+    let theRole = message.guild.roles.find("name", 'rainbow');
+
     function discoRole() {
         let roles = ['rainbow']
         let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
@@ -12,8 +15,10 @@ module.exports.run = async (Discord, client, message, args) => {
         });
     }
 
-    if(message.author.id === '498479906439823370') {
+    if(message.author.id === '498479906439823370' && !giverole) {
     setInterval(() => { discoRole(); }, 60000);
     message.channel.send("```I'm rainbowing the role```");
- }
+ }else if (message.author.id === '498479906439823370' && giverole) {
+  giverole.addRole(theRole).then(() => message.channel.send('I gave the rainbow role to **'+giverole+'**'));
+}
 };
