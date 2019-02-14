986678952404c33800ec1f39016d79ec4d944972
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-02-14 12:20:11 +0100

    Update kiss.js

--- command/kiss.js
+++ command/kiss.js
@@ -2,7 +2,7 @@ const superagent = require("superagent");
 exports.run = async (Discord, client, message, args) => {
 
     let Hugser = message.mentions.users.first();
-
+    let me = args[0];
     let hugger = message.author;
     let roleColor = message.member.highestRole.color;
     let emote = client.emojis.find(e=>e.name === 'TohsakaFacepalm')
@@ -11,7 +11,19 @@ exports.run = async (Discord, client, message, args) => {
 
     if(Hugser === message.author) return message.channel.send('Lemme laugh to see you kiss yourself ')
     
-    if(Hugser.id === client.user.id) return message.channel.send('I can\'t kiss you');
+    if(Hugser.id === client.user.id)  return message.channel.send('I can\'t kiss you');
+    
+    if(me === 'me') {
+    const {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/kiss`);
+        
+    let embed = new Discord.RichEmbed()
+    .setDescription(`A kiss just for you Master !`)
+    .setImage(body.url)
+    .setColor(roleColor)
+    message.channel.send(embed)
+   }
+        
 
     const {body} = await superagent
     .get(`https://nekos.life/api/v2/img/kiss`);
