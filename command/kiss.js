c701af2e8436e774c14bcbb60940a67b07b42468
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-02-14 12:44:52 +0100

    Update kiss.js

--- command/kiss.js
+++ command/kiss.js
@@ -13,13 +13,27 @@ exports.run = async (Discord, client, message, args) => {
     
     if(Hugser.id === client.user.id)  return message.channel.send('I can\'t kiss you');
          
-
-    const {body} = await superagent
+    if(!Hugser && !me) return message.channel.send(`Trying to kiss what ${emote}`);
+    
+    if(me === 'me' && message.author.id === '498479906439823370') {
+        const {body} = await superagent
     .get(`https://nekos.life/api/v2/img/kiss`);
 
     let embed = new Discord.RichEmbed()
-    .setDescription(`**${hugger.username}** gives a kiss to **${Hugser.username}**, how cute they are !`)
+    .setDescription(`Yeah, I kiss Master !`)
     .setImage(body.url)
-    .setColor(roleColor)
     message.channel.send(embed)
+    } else if (me === 'me' && message.author.id !== '498479906439823370') {
+        return message.channel.send('Hum, no sorry')
+    }
+
+    const {body} = await superagent
+    .get(`https://nekos.life/api/v2/img/kiss`);
+    if(Hugser !== client.user.id) {
+        let embed = new Discord.RichEmbed()
+        .setDescription(`**${hugger.username}** gives a kiss to **${Hugser.username}**, how cute they are !`)
+        .setImage(body.url)
+        .setColor(roleColor)
+        message.channel.send(embed)
+    }
 }
