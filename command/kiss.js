363369079b9f015011fcbb2b76d4ccdbfef8d106
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-02-14 12:48:53 +0100

    Update kiss.js

--- command/kiss.js
+++ command/kiss.js
@@ -2,36 +2,39 @@ const superagent = require("superagent");
 exports.run = async (Discord, client, message, args) => {
 
     let Hugser = message.mentions.users.first();
-    let me = args[0];
-    let hugger = message.author;
+    var me = args[0];
     let roleColor = message.member.highestRole.color;
-    let emote = client.emojis.find(e=>e.name === 'TohsakaFacepalm')
+    let hugger = message.author;
 
-    if(Hugser === message.author) return message.channel.send('Lemme laugh to see you kiss yourself ')
-    
-    if(Hugser.id === client.user.id)  return message.channel.send('I can\'t kiss you');
-         
-    if(!Hugser && !me) return message.channel.send(`Trying to kiss what ${emote}`);
-    
+    let emote = client.emojis.find(e=>e.name === 'TohsakaFacepalm')
     if(me === 'me' && message.author.id === '498479906439823370') {
         const {body} = await superagent
     .get(`https://nekos.life/api/v2/img/kiss`);
 
     let embed = new Discord.RichEmbed()
     .setDescription(`Yeah, I kiss Master !`)
+    .setColor(roleColor)
     .setImage(body.url)
     message.channel.send(embed)
     } else if (me === 'me' && message.author.id !== '498479906439823370') {
         return message.channel.send('Hum, no sorry')
     }
+    
+    if(!Hugser && !me) return message.channel.send(`Trying to kiss what ${emote}`)
 
+    if(Hugser.id === '469099157269774337') {
+        return message.channel.send('Trying to do what')
+    } 
+
+    if(Hugser === message.author) return message.channel.send('Lemme laugh to see you kiss yourself ')
+    if(Hugser !== client.user.id) {
     const {body} = await superagent
     .get(`https://nekos.life/api/v2/img/kiss`);
-    if(Hugser !== client.user.id) {
-        let embed = new Discord.RichEmbed()
-        .setDescription(`**${hugger.username}** gives a kiss to **${Hugser.username}**, how cute they are !`)
-        .setImage(body.url)
-        .setColor(roleColor)
-        message.channel.send(embed)
+
+    let embed = new Discord.RichEmbed()
+    .setDescription(`**${hugger.username}** gives a kiss to **${Hugser.username}**, how cute they are !`)
+    .setColor(roleColor)
+    .setImage(body.url)
+    message.channel.send(embed)
     }
 }
