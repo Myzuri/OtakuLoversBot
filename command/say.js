d27f78e0431ebb335b3d92b0dfe3253928011876
OtakLov <46738962+OtakLov@users.noreply.github.com>
2019-03-27 21:18:41 +0100

    Update say.js

--- command/say.js
+++ command/say.js
@@ -2,9 +2,9 @@ module.exports.run = async (Discord, client, message, args) =>  {
 
 	var say = args.join(' ')
 
-	if(!say [0] || say[0 == 'help']) return message.channel.send("```!say [content] \n cannot be empty | don't even try !dsay [!say]```")
+	if(!say [0] || say[0 == 'help']) return message.channel.send("```;say [content] \n cannot be empty | don't even try ;say [;say]```")
 	
-		message.delete(1000)
+		message.delete()
 		return message.channel.send(say)
 	
 }
