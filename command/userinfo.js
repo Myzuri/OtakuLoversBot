38d28d5f72e2dbefd3d668297538d2b74295a9e7
Noël <46738962+Myzuri@users.noreply.github.com>
2019-06-14 11:49:14 +0200

    Update userinfo.js

--- command/userinfo.js
+++ command/userinfo.js
@@ -8,10 +8,10 @@ module.exports.run = async (client, args, message) => {
       .setColor(roleColor)
       .setTitle("Here some info about " + `${user.tag}`)
       .setThumbnail(user.displayAvatarURL)
-      .addField("Etat", user.presence.status, true)
+      .addField("Status", giveMeAStatus(user.presence.status), true)
       .addField("ID", user.id, true)
       .setDescription('[full image]('+user.avatarURL+')')
-      .addField("Game", "\`" + user.presence.game.name + "\`")
+      .addField("Game", "\`" + gamePlaying(user.presence.game) + "\`")
       .setFooter("Account created at : ")
       .setTimestamp(user.createdAt)
       return message.channel.send(embed);
