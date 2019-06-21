97c20ec89b6cd1b474140d458c59f58939ebfb70
Noël <46738962+Myzuri@users.noreply.github.com>
2019-06-21 08:48:04 +0200

    Update help.js

--- command/help.js
+++ command/help.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, args, message) => {
     let user = message.author;
     let sembed = new Discord.RichEmbed()
     .setTitle("My commands")
-    .addField('Fun', '`avatar`, `ascii`, `calc`, `ping`, `cat`, `shifumi`, `hug`, `8ball`, `kiss`')
+    .addField('Fun', '`avatar`, `ascii`, `calc`, `ping`, `cat`, `shifumi`, `hug`, `8ball`, `kiss`, `pat`')
     .addField('Usefull', '`userinfo`, `serverinfo`, `purge`')
     .setColor(roleColor)
 
