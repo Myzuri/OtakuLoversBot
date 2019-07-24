5a9d7e21638551d489613082aed220278379477b
Noël <46738962+Myzuri@users.noreply.github.com>
2019-07-24 09:46:51 +0200

    Update avatar.js

--- command/avatar.js
+++ command/avatar.js
@@ -14,7 +14,7 @@ module.exports.run = async (Discord, client, message, args) => {
 
 			let embed = new Discord.RichEmbed()
 			.setColor(roleColor)
-			.setDescription(`${user.tag}'s avatar`)
+			.setDescription(`[${user.tag}'s avatar](${user.avatarURL})`)
 			.setImage(`${user.avatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
 
@@ -23,14 +23,14 @@ module.exports.run = async (Discord, client, message, args) => {
 
 			let embed = new Discord.RichEmbed()
 			.setColor(roleColor)
-			.setDescription(`${tags.tag}'s avatar`)
+			.setDescription(`[${tags.tag}'s avatar](${tags.avatarURL})`)
 			.setImage(`${tags.avatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
 		} else {
 			var author = message.author;
 			let embed = new Discord.RichEmbed()
 			.setColor(roleColor)
-			.setDescription("Your avatar")
+			.setDescription('[Your avatar]('+message.author.avatarURL+')')
 			.setImage(`${author.displayAvatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${message.author.tag} used avatar`))
 		
