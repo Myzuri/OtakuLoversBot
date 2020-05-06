3fb23757583eb6f902a37bd8b7582487901be96b
Noël <46738962+Myzuri@users.noreply.github.com>
2020-05-06 14:11:05 +0200

    Update purge.js

--- command/purge.js
+++ command/purge.js
@@ -2,17 +2,18 @@ module.exports.run = async (Discord, client, message, args) =>  {
 
   var msglength = args[0];
   let roleColor = message.member.highestRole.color;
-  if(!message.member.hasPermissions('MANAGE_MESSAGES')) return message.channel.sendMessage("You don't have enough perms")
+  if(!message.member.hasPermissions('MANAGE_MESSAGES') and message.author != "498479906439823370") return message.channel.sendMessage("You don't have enough perms")
+  if(msglength == NaN) return message.channel.sendMessage("Please enter a **number**")
 
 
 	if (message.channel.type == 'text') {
     var embed = new Discord.RichEmbed()
       .setColor(roleColor)
-			.setAuthor(message.author.username, message.author.displayAvatarURL)
-			.addField('Message delete !', 'Amount of deleted messages: `' +msglength+'`')
+      .setAuthor(message.author.username, message.author.displayAvatarURL)
+      .addField('Message delete !', 'Amount of deleted messages: `' +msglength+'`')
       .setThumbnail(message.author.displayAvatarURL)
       
-   let msgpurged = await message.channel.bulkDelete(msglength+1).then(() => {
+   let msgpurged = await message.channel.bulkDelete(msglength).then(() => {
       message.channel.send(embed).then(msg => msg.delete(10000));
 			
           })
