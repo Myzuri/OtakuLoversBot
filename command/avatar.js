b7efab3dbb6d7e0ce79394fdb9da2c990f85e9f7
Noël <46738962+Myzuri@users.noreply.github.com>
2021-06-07 10:07:57 +0200

    Update avatar.js

--- command/avatar.js
+++ command/avatar.js
@@ -6,14 +6,14 @@ module.exports.run = async (Discord, client, message, args) => {
 	let user = message.mentions.users.first();
 	let search = args.join(' ');
 	let tags = client.users.find(u => u.username === search);
-	let roleColor = message.member.highestRole.color;
+	let randomColor = Math.floor(Math.random()*16777215).toString(16);
 	var author; // mention to get avatar, or type username
 	
 		if(user){
 			var author = user;
 
 			let embed = new Discord.RichEmbed()
-			.setColor(roleColor)
+			.setColor(randomColor)
 			.setDescription(`[${user.tag}'s avatar](${user.avatarURL})`)
 			.setImage(`${user.avatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
@@ -22,14 +22,14 @@ module.exports.run = async (Discord, client, message, args) => {
 			var author = tags;
 
 			let embed = new Discord.RichEmbed()
-			.setColor(roleColor)
+			.setColor(randomColor)
 			.setDescription(`[${tags.tag}'s avatar](${tags.avatarURL})`)
 			.setImage(`${tags.avatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${avuser.tag} used avatar on ${author.tag}`))
 		} else {
 			var author = message.author;
 			let embed = new Discord.RichEmbed()
-			.setColor(roleColor)
+			.setColor(randomColor)
 			.setDescription('[Your avatar]('+message.author.avatarURL+')')
 			.setImage(`${author.displayAvatarURL}`)
 			return message.channel.send(embed).then(() => console.log(`${message.author.tag} used avatar`))
