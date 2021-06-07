ea6791090ee6d0e3a65fbf54792e2e4a8dcec1f3
Noël <46738962+Myzuri@users.noreply.github.com>
2021-06-07 10:08:45 +0200

    Update help.js

--- command/help.js
+++ command/help.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js')
 
 module.exports.run = async (client, args, message) => {
-    let roleColor = message.member.highestRole.color
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
     let user = message.author;
     let sembed = new Discord.RichEmbed()
     .setTitle("My commands")
     .addField('Fun', '`avatar`, `ascii`, `calc`, `ping`, `cat`, `shifumi (WIP)`, `hug`, `8ball`, `kiss`, `pat`, `pic`, `slain (WIP)`, `dab`, `jr`')
     .addField('Usefull', '`userinfo`, `serverinfo`, `purge`')
-    .setColor(roleColor)
+    .setColor(randomColor)
 
     //.setTimestamp(message.guild.createdAt);
     return message.channel.send(sembed);
